0999f85698eb4c6684f6598c88c7f866643a796f
Jeff Andersen <jeffandersen@google.com>
2025-04-21 07:07:35 +0000

    Shuffle variable definitions.

--- ocp_docker_run.sh
+++ ocp_docker_run.sh
@@ -3,8 +3,6 @@
 OCP_CONTACT="https://www.opencompute.org/contact"
 RESOURCE_DIR_NAME=ocp-pandoc-resources
 
-docker_image=${DOCKER_IMAGE:-"ghcr.io/trustedcomputinggroup/pandoc:latest"}
-
 print_usage() {
   echo "Usage:"
   echo "$(basename "${0}") [options] [build.sh arguments]"
@@ -24,13 +22,15 @@ if test "${1}" == "--help"; then
   exit 0
 fi
 
-RESOURCE_DIR="$(realpath $(dirname "$0")/${RESOURCE_DIR_NAME})"
+resource_dir="$(realpath $(dirname "$0")/${RESOURCE_DIR_NAME})"
+
+docker_image=${DOCKER_IMAGE:-"ghcr.io/trustedcomputinggroup/pandoc:latest"}
 
 echo "Launching container: ${docker_image}"
 docker run \
   --workdir=/workspace \
   --volume=$(pwd):/workspace \
-  --volume=${RESOURCE_DIR}:/extra_resources/${RESOURCE_DIR_NAME} \
+  --volume=${resource_dir}:/extra_resources/${RESOURCE_DIR_NAME} \
   "${docker_image}" \
   --crossref=tcg \
   --metadata_contact "${OCP_CONTACT}" \
